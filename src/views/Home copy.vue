842a9a2af2428357d995f253ca4c36190db2a25d
Басун Володимир <vbasoon@gmail.com>
2021-04-07 17:50:04 +0300

    working

---
+++ src/views/Home copy.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data: (()=>{
+    starWarsFilms:[]
+  }),
+  methods: {
+    getStarWarsFilms = () => {
+      fetch(`https://swapi.dev/api/films/`)
+        .then((res) => res.json())
+        .then((data) => {
+          setFilms(data.results);
+        });
+    },
+    getStarWarsPeople = () => {
+      fetch(`https://swapi.dev/api/people/`)
+        .then((res) => res.json())
+        .then((data) => {
+          setPeople(data.results);
+        });
+    },
+    getStarWarsPlanets = () => {
+      fetch(`https://swapi.dev/api/planets/`)
+        .then((res) => res.json())
+        .then((data) => {
+          setPlanets(data.results);
+        });
+    }
+  },
+  components: {
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>This is a Home page</h1>
+    <h2>Films:</h2>
+    <ul>
+      <li v-for="film in films" :key="film.title" class="col">
+        <div class="card">
+          <div class="card__content">
+            <div class="card__header">
+              <h2>{{ film.title }}</h2>
+              <h3>{{ film.director }}</h3>
+              <strong>
+                <p>{{ film.release_date }}</p>
+              </strong>
+            </div>
+            <div class="card__desc">
+              <strong>Description</strong>
+              <p>{{ film.opening_crawl }}</p>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      films: [],
+    };
+  },
+  mounted() {
+    fetch(`https://swapi.dev/api/films/`)
+      .then((res) => res.json())
+      .then((data) => (this.films = data.results))
+      .catch((err) => console.log(err.message));
   },
 };
 </script>
+
+<style scoped>
+li {
+  list-style-type: none;
+}
+</style>
